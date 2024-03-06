2aad836180087cfbfc9d00207974c6e455817708
nhansen23 <156024214+nhansen23@users.noreply.github.com>
2024-03-05 21:40:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from shiny.express import ui, input, render
+import seaborn as sns
+from sklearn.datasets import load_iris
 
 # Requirement 1: Page title
 ui.page_opts(title="My Browser Interactive App", fillable=True)
@@ -13,19 +15,23 @@ ui.page_opts(title="My Browser Interactive App", fillable=True)
 # int for max number of bins
 # int for initial slider value
 with ui.sidebar():
-  ui.input_slider("number_of_bins", "Number of Bins", 0, 50, 25)
-
+    ui.input_slider("number_of_bins", "Number of Bins", 0, 50, 25)
 
 # Requirement 3: reactive output chart
 # pass numpy data array
 # pass user input of number of bins
 # set density to True
 @render.plot(alt="A histogram")
-def draw_histogram():
-  count_of_points: int = 437
-  np.random.seed(10)
-  random_data_array = 100 + 15 * np.random.randn(count_of_points)
-  plt.hist(random_data_array, input.number_of_bins(), density=True)
-  random_data_array = 100 + 15 * np.random.randn(count_of_points)
-  plt.hist(random_data_array, input.number_of_bins(),density=True)
+def histogram():
+    count_of_points: int = 437
+    np.random.seed(10)
+    random_data_array = 100 + 15 * np.random.randn(count_of_points)
+    plt.hist(random_data_array, input.number_of_bins(), density=True)
 
+@render.plot(alt="A scatterplot")
+def scatterplot():
+    iris = load_iris()
+    sns.relplot(
+        iris, x="petal_width", y="petal_length", hue="petal_length", style="species"
+    )
+    plt.show()
